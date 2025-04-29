478179790bff4e115909848d02ef190d360e12aa
santanna37 <santanna.ilha@gmail.com>
2025-04-29 16:47:10 -0300

    c2 - CONFIG:Instalação e cconfiguração do alembic- automação de alteração no banco de dados

---
+++ src/infra/database/entities/__init__.py
@@ -0,0 +1,1 @@
+from .entities_contador import ContadorEntitie
---
+++ src/infra/database/entities/entities_contador.py
@@ -0,0 +1,13 @@
+from sqlalchemy import Column, Integer, String
+from src.infra.database.setting.base import Base
+
+
+class ContadorEntitie(Base):
+    __tablename__ = "contador"
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    nome = Column(String(100), nullable=False)
+    email = Column(String(100))
+
+    def __repr__(self):
+        return f"Contador [id={self.id}, nome={self.nome}]"
